a8e4bd6a33fb9869e25e5ad1ecbb5943f48900ed
Blinston S. Fernandes <b2ornot2b@gmail.com>
2016-10-14 11:20:17 +0530

    screen manager

--- genericui.py
+++ genericui.py
@@ -1,19 +1,22 @@
 from __future__ import print_function
 
-from kivy.uix.floatlayout import FloatLayout
+
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 from kivy.clock import Clock
 from kivy.logger import Logger
 import functools
 
 from keyboard import Keyboard
 
-class GenericUI(FloatLayout):
+class GenericUI(BoxLayout):
     def __init__(self, *a, **k):
+        k.setdefault('cols', 2)
         super(GenericUI, self).__init__(*a, **k)
-        ti = TextInput()
-        self.add_widget(ti)
         self.bind(on_barcode_scan=self.on_barcode_scan)
+        self.create_ui()
 
     def on_size(self, w, size):
         orientation = 'portrait' if size[0] < size[1] else 'landscape'
@@ -23,3 +26,15 @@ class GenericUI(FloatLayout):
     def on_barcode_scan(self, barcode, *args):
         Logger.info('GenericUI.on_barcode_scan {}'.format(barcode))
 
+    def create_ui(self):
+        self.sm = ScreenManager()
+        self.sm.add_widget(self.get_home_screen())
+        self.add_widget(self.sm)
+        #ti = Input()
+        #self.add_widget(ti)
+
+    def get_home_screen(self):
+        home = Screen(title="Home")
+        btn = Button(text="OK")
+        home.add_widget(btn)
+        return home
